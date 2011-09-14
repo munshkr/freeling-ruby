6fbf888035fda42c5237b5ef97bf50e865b6c88f
Damián Silvani <munshkr@gmail.com>
2011-09-14 11:16:18 -0300

    bundle gem initialization

---
+++ lib/freeling-ruby.rb
@@ -0,0 +1,7 @@
+require "freeling-ruby/version"
+
+module FreeLing
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/freeling-ruby/version.rb
@@ -0,0 +1,5 @@
+module FreeLing
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
